1d2a580ef8970376c546c89463179b2164525354
Kenneth Watson <git@shrimpworks.za.net>
2019-12-27 20:32:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Minimum Effort Search
+
+A simple REST service exposing some RedisSearch functionality to support
+simple self-hosted site search functionality.
---
+++ src/test/java/net/shrimpworks/mes/MainTest.java
@@ -0,0 +1,13 @@
+package net.shrimpworks.mes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class MainTest {
+
+	@Test
+	public void todo() {
+		fail("todo");
+	}
+}
